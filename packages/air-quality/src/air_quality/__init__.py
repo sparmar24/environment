0113f8c765971e8edcc70d762e300a7b396c7907
Sonia Parmar <soniaparmar24@gmail.com>
2025-07-08 20:19:57 +0200

    code: call public API of air quality data

---
+++ packages/air-quality/src/air_quality/__init__.py
@@ -0,0 +1,30 @@
+import requests
+import os
+import json
+
+def main() -> None: 
+    APIKEY = os.environ.get("API_KEY") 
+    url = f"https://airquality.googleapis.com/v1/currentConditions:lookup?key={APIKEY}"
+
+    # JSON payload
+    payload = {
+        "location": {
+            "latitude": 37.419734,
+            "longitude": -122.0827784
+        }
+    }
+
+    # HTTP headers
+    headers = {
+        "Content-Type": "application/json"
+    }
+
+    # Make the POST request
+    response = requests.post(url, json=payload, headers=headers)
+
+    # Print the response
+    if response.status_code == 200:
+        print(response.json())
+    else:
+        print("Error:", response.status_code, response.text)
+
